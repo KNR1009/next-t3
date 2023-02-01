ef45f31b1ad06743561daa68881c0cf321b5757b
伊東和成 <itokazunari5375@icloud.com>
2023-02-01 23:48:11 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // 一旦false
+  reactStrictMode: false,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
