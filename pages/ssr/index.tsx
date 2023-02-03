c6091bc4c6398666061fff064baec7a73d817ac9
伊東和成 <itokazunari5375@icloud.com>
2023-02-04 08:54:32 +0900

    feat: ssrを追加

--- pages/ssr/index.tsx
+++ pages/ssr/index.tsx
@@ -1,3 +1,48 @@
-export default function SSR() {
-  console.log("hello");
-}
+import axios from "axios";
+import type { GetServerSideProps, NextPage } from "next";
+import React from "react";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const SSRPage: NextPage<{ posts: Post[] }> = (props) => {
+  return (
+    <>
+      {props.posts &&
+        props.posts.map((post) => (
+          <div key={post.id}>
+            <p>
+              {post.id}:{post.title}
+            </p>
+          </div>
+        ))}
+    </>
+  );
+};
+export default SSRPage;
+
+// サーバサイドで実行する処理(getServerSideProps)を定義する
+export const getServerSideProps: GetServerSideProps<{ posts: Post[] }> = async (
+  context
+) => {
+  console.log("getServerSideProps");
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  const result = await res.data;
+
+  const posts: Post[] = result.map((i: any) => {
+    return {
+      userId: i.userId,
+      id: i.id,
+      title: i.title,
+      body: i.body,
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
