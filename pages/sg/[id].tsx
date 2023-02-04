972fe5c4f9ac97d71317c6cb280d9afda1448fc0
伊東和成 <itokazunari5375@icloud.com>
2023-02-04 23:38:56 +0900

    feat: ダイナミックルートの作成

--- pages/sg/[id].tsx
+++ pages/sg/[id].tsx
@@ -1,34 +1,42 @@
+import axios from "axios";
 import { GetStaticProps, NextPage } from "next";
 
-const SGPage: NextPage<{ id: number }> = (props) => {
-  return <p>このページは{props.id}</p>;
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+  created_at: string;
+  updated_at: string;
+};
+const SGPage: NextPage<{ post: Post }> = (props) => {
+  return <p>このページは{props.post.title}</p>;
 };
 
 export default SGPage;
 
-// ダイナミックルートのパスを指定する
+// ダイナミックルートのid一覧を取得する
 export const getStaticPaths = async () => {
+  const result = await axios
+    .get("http://127.0.0.1:3005/posts")
+    .then((res) => res.data);
+  if (!result) return;
+  const paths = result.map((i: Post) => ({
+    params: { id: String(i.id) },
+  }));
+
   return {
-    paths: [
-      {
-        params: {
-          id: "1",
-        },
-      },
-      {
-        params: {
-          id: "2",
-        },
-      },
-    ],
+    paths,
     fallback: false,
   };
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  console.log("SG");
-
+export const getStaticProps: GetStaticProps<{ post: Post }> = async ({
+  params,
+}) => {
+  const post = await axios
+    .get(`http://127.0.0.1:3005/posts/${params?.id}`)
+    .then((res) => res.data);
   return {
-    props: { id: params?.id },
+    props: { post },
   };
 };
